3b23f278767c759d550961f7b75caecc6e751c59
trayvon21 <718869429@qq.com>
2020-03-27 13:38:31 +0800

    init: 创建项目
    1、封装接口文件
    2、创建路由

---
+++ pages/index/index.vue
@@ -0,0 +1,37 @@
+<template>
+	<view class="container">
+		{{banner}}
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				banner: null
+			}
+		},
+		methods: {
+			getData() {
+				this.$api.getIndex().then(res => {
+					console.log(res.data);
+					if(res.data.success){
+						this.data=res.data
+					}
+				}).catch(err => {
+
+				})
+			}
+		},
+		onLoad() {
+			this.getData()
+		}
+	}
+</script>
+
+<style>
+	.intro {
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
